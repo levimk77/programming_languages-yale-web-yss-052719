1cfb281b5a35088744908e1405b3ed037020c232
Elijah Florence <efloren@siue.edu>
2019-05-19 21:43:27 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,7 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |languagetype, languageName|
-    languageName.each do |languageType, 
+    languageName.each do |languageType, language|
+      language.each do |key, value|
+        new_hash << key: 
 end
